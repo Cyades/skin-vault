bf83f0e490c291067bd9e4069cb4c9ee880b7d2d
Malvin Scafi <154596305+Cyades@users.noreply.github.com>
2024-09-10 13:35:24 +0700

    fix: Deployment URLs

--- skin_vault/settings.py
+++ skin_vault/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j)-(q_6r+(&-^mqj=fqgcts-z57nqj$swgmmndk*x0$x+fq&t^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "skinvault.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "malvin-scafi-skinvault.pbp.cs.ui.ac.id"]
 
 
 # Application definition
